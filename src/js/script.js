52b5e25b036844037785aa26a8e3c2be465295dd
jovencio neto <151071159+JovencioNeto@users.noreply.github.com>
2024-05-23 13:50:50 -0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -10,51 +10,50 @@ button_submit.addEventListener('click', () =>{
     const msn = document.getElementById('msn')
 
     const validation =  new RegExp((/\S+@\S+\.\S+/))
+   
+    if(input_name.value == 0 || input_email.value == 0 || msn.value == 0) {
+        if (input_name.value == 0 && input_email.value == 0 && msn.value == 0){
 
-    if(validation.test(input_email.value)){
-        if(input_name.value == 0 || input_email.value == 0 || msn.value == 0) {
-            if (input_name.value == 0 && input_email.value == 0 && msn.value == 0){
-    
-                input_name.setAttribute("required", "")
-                input_email.setAttribute("required", "")
-                msn.setAttribute("required", "")
-    
-                alert('Nenhum dos campos foram preenchidos!')
-    
-            }else if (input_name.value == 0) {
-    
-                input_name.setAttribute("required", "")
-                alert('O campo do nome não foi preenchido!')
-    
-            } else if (input_email.value == 0){
-    
-                input_email.setAttribute("required", "")
-                alert("O campo do email não foi preenchido!")
-            } else if(msn.value == 0){
-    
-                msn.setAttribute("required", "")
-                alert("O campo de mensagem não foi preenchido!")
-            }
-        } else{
-            
+            input_name.setAttribute("required", "")
+            input_email.setAttribute("required", "")
+            msn.setAttribute("required", "")
+
+            alert('Nenhum dos campos foram preenchidos!')
+
+        }else if (input_name.value == 0) {
+
+            input_name.setAttribute("required", "")
+            alert('O campo do nome não foi preenchido!')
+
+        } else if (input_email.value == 0){
+
+            input_email.setAttribute("required", "")
+            alert("O campo do email não foi preenchido!")
+        } else if(msn.value == 0){
+
+            msn.setAttribute("required", "")
+            alert("O campo de mensagem não foi preenchido!")
+        }}
+    else{
+        if (validation.test(input_email.value)){
             alert("O formulário foi preenchido com sucesso!" +
                 "\nNome: " + input_name.value +
                 "\nEmail: " + input_email.value +
                 "\nMensagem: " + msn.value)
-    
+        
             input_name.value = ""
             input_email.value = ""
             msn.value = ""
-    
+        
             input_name.removeAttribute("required")
             input_email.removeAttribute("required")
             msn.removeAttribute("required")
+        }else{
+            alert("Email Invalido! Adicione um '@' e após o '@' adicione um '.', assim como um conteúdo antes do '.' e após o '.'")
         }
-    } else{
-        alert("Email Inválido")
     }
-
 })
+
 
 form.addEventListener('submit', (ev) =>{
     ev.preventDefault()
